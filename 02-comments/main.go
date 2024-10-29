c063c9ba46eacf5b3411796190bfd2fae92a6a95
Burak Özcan <b@ozsan.tr>
2024-10-29 14:39:05 +0300

    Update main.go

--- 02-comments/main.go
+++ 02-comments/main.go
@@ -1,13 +1,13 @@
 //Declare the package name
 
+/*
+ Go Programming Language
+ multi line comment block
+*/
+
 package main
 
 import "fmt"
-
-/*
- In Go Programming Language
- main executable application must be start with package main
-*/
 
 func main() {
 	//We are using the 'fmt' package name and 'Println' function to print something on the screen
