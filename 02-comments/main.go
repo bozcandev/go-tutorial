c196c99e969ee0de47438503b216d1414534c46b
Burak Özcan <b@ozsan.tr>
2024-10-29 13:16:34 +0300

    Create main.go

---
+++ 02-comments/main.go
@@ -0,0 +1,15 @@
+//Declare the package name
+
+package main
+
+import "fmt"
+
+/*
+ In Go Programming Language
+ main executable application must be start with package main
+*/
+
+func main() {
+	//We are using the 'fmt' package name and 'Println' function to print something on the screen
+	fmt.Println("Comments ...")
+}
