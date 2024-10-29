6e176d339789ec1c6e5a6ca0567ed50dbbdb0faa
Burak Özcan <b@ozsan.tr>
2024-10-29 15:20:00 +0300

    Create main.go

---
+++ 04-constants/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// constant can not ve change like a variable
+const version = 0.1
+
+func main() {
+	fmt.Println("Application Version is : ", version)
+	// we can not change constant
+	// version = 0.2
+}
