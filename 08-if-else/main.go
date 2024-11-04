25a316a884b5a3318c90f90136c7697c1130e456
Burak Özcan <b@ozsan.tr>
2024-11-04 15:39:26 +0300

    update

---
+++ 08-if-else/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var superNumber int = 10
+
+	if superNumber < 10 {
+		fmt.Println("A is smaller then 10")
+	} else {
+		fmt.Println("A is equal or bigger then 10")
+	}
+}
---
+++ 09-switch-case/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "go" //please change "go" to "c"
+
+	switch name {
+	case "go":
+		fmt.Println("GO")
+	case "c":
+		fmt.Println("C")
+	case "c++":
+		fmt.Println("c++")
+	}
+}
