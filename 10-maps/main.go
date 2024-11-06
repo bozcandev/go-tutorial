59688f941a679cb7da5db0b51c9e9d077c8a9d9a
Burak Özcan <b@ozsan.tr>
2024-11-06 12:16:39 +0300

    Update main.go

--- 10-maps/main.go
+++ 10-maps/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 
-	var cars = map[int]string{0: "Alfa Rome", 1: "BMW"}
+	var cars = map[int]string{0: "Alfa Romeo", 1: "BMW"}
 	fmt.Println(cars)
+
+	fmt.Println("***")
+
+	fmt.Println(cars[0])
 
 }
