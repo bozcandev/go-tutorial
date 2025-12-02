c7dda6a874c23b77391105a0b1361dfc27ba769a
Burak Ozcan <b@ozsan.tr>
2025-12-02 13:12:00 +0300

    initial commit

---
+++ 01-variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	a = "Hello"
+	var b = "World"
+	fmt.Println(a, b)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bozcandev/go-tutorial
+
+go 1.25.3
